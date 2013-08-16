c4645719be36ccd2e0689444af46c332a87e69f4
Craig Vermeer <craig@vermeers.net>
2013-08-16 06:54:59 -0400

    Skeleton web app

---
+++ Samples/SampleWebApp/SampleWebApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SampleWebApp.Models;
+
+namespace SampleWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("SayHello", new NameInputModel());
+        }
+
+        [HttpPost]
+        public ActionResult SayHello(NameInputModel id)
+        {
+            return View(id);
+        }
+
+        public ActionResult SayHello()
+        {
+            return View();
+        }
+    }
+}
