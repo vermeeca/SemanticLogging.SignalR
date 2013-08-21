b4f94d330320c04ffcae293601e8d755eea95e8f
Craig Vermeer <craig@vermeers.net>
2013-08-21 07:01:01 -0400

    SignalR

--- src/SemanticLogging.SignalR/SignalRLog.cs
+++ src/SemanticLogging.SignalR/SignalRLog.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics.Tracing;
+using System.Web;
+using System.Web.Hosting;
 using Microsoft.Practices.EnterpriseLibrary.SemanticLogging;
 using Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Formatters;
 using Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Utility;
@@ -9,7 +11,7 @@ namespace SemanticLogging.SignalR
     public static class SignalRLog
     {
         /// <summary>
-        /// Subscribes to an <see cref="T:System.IObservable`1"/> using a <see cref="T:Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Sinks.ConsoleSink"/>.
+        /// Subscribes to an <see cref="T:System.IObservable`1"/> using a <see cref="T:SemanticLogging.SignalR.SignalRSink"/>.
         /// 
         /// </summary>
         /// <param name="eventStream">The event stream. Typically this is an instance of <see cref="T:Microsoft.Practices.EnterpriseLibrary.SemanticLogging.ObservableEventListener"/>.</param><param name="formatter">The formatter.</param><param name="colorMapper">The color mapper instance.</param>
@@ -21,16 +23,23 @@ namespace SemanticLogging.SignalR
             var sink = new SignalRSink();
             formatter = formatter ?? new JsonEventTextFormatter();
 
+            EnsureHostConfigured(sink);
+
             return new SinkSubscription<SignalRSink>(EventEntryExtensions.SubscribeWithFormatter(eventStream, formatter, sink), sink);
         }
 
+        private static void EnsureHostConfigured(SignalRSink sink)
+        {
+            sink.StartHost();
+        }
+
         /// <summary>
-        /// Creates an event listener that logs using a <see cref="T:Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Sinks.ConsoleSink"/>.
+        /// Creates an event listener that logs using a <see cref="T:SemanticLogging.SignalR.SignalRSink"/>.
         /// 
         /// </summary>
-        /// <param name="formatter">The formatter.</param><param name="colorMapper">The color mapper instance.</param>
+        /// <param name="formatter">The formatter.</param>
         /// <returns>
-        /// An event listener that uses <see cref="T:Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Sinks.ConsoleSink"/> to display events.
+        /// An event listener that uses <see cref="T:SemanticLogging.SignalR.SignalRSink"/> to display events.
         /// </returns>
         public static EventListener CreateListener(IEventTextFormatter formatter = null)
         {
--- src/SemanticLogging.SignalR/SignalRSink.cs
+++ src/SemanticLogging.SignalR/SignalRSink.cs
@@ -3,30 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Owin.Hosting;
 using Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Sinks;
 
 namespace SemanticLogging.SignalR
 {
     public class SignalRSink : IObserver<string>
     {
+        private IDisposable host;
+        private IHubConnectionContext clients;
+
         public SignalRSink()
         {
             
         }
 
+        internal void StartHost()
+        {
+            this.host = WebApp.Start<Startup>(@"http://localhost:12345");
+            this.clients = GlobalHost.ConnectionManager.GetHubContext<SemanticLoggingHub>().Clients;
+        }
+
         public void OnNext(string value)
         {
+            clients.All.MessageLogged(value);
             System.Diagnostics.Debug.WriteLine(value);
         }
 
         public void OnError(Exception error)
         {
+            clients.All.ErrorOccurred(error);
             System.Diagnostics.Debug.WriteLine(error);
         }
 
         public void OnCompleted()
         {
             System.Diagnostics.Debug.WriteLine("Completed");
+            if (host != null)
+            {
+                host.Dispose();
+            }
         }
     }
 }
